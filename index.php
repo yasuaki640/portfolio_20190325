9704ca711a24242b93b23e20d4fd6cfb0c479b77
yasuaki640 <yasuaki640@gmail.com>
2019-03-26 11:09:51 +0900

    画像を読み込み

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php require_once(dirname(__FILE__) .'/layouts/header.php') ?>
+
+<div class="container-fluid mx-auto my-4" style="max-width: 800px">
+    <img src="layouts/images/tsubasa.png">
+</div>
+
+<?php require_once(dirname(__FILE__) .'/layouts/footer.php') ?>
