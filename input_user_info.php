d9c28869a201bc20a57a2721ec82a2a68b06c8d2
yasuaki640 <yasuaki640@gmail.com>
2019-04-03 20:35:51 +0900

    レイアウトの調整

--- input_user_info.php
+++ input_user_info.php
@@ -1,16 +1,11 @@
-<?php
-
-unset($_SESSION['user']);
-
-?>
-
 <?php require_once(dirname(__FILE__) . '/layouts/header.php') ?>
 
     <div class="container-fluid mx-auto my-4" style="max-width: 350px">
         <form action="do_create_user.php" method="post">
-            <p>ユーザーネーム<input type="text" name="username"></p>
-            <p>パスワード<input type="text" name="password"></p>
-            <p>パスワード(確認用)<input type="text" name="password_confirm"></p>
+            <p>ユーザーネーム<input type="text" name="name"></p>
+            <p>パスワード<input type="password" name="password"></p>
+            <p>パスワード(確認用)<input type="password" name="password_confirm"></p>
+            <input type="submit" value="新規登録">
         </form>
     </div>
 
