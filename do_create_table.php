7a9ad819e151b720604c4213f1a0c82bfc269a2f
yasuaki640 <yasuaki640@gmail.com>
2019-05-11 11:10:17 +0900

    ログインボタンを追加

--- do_create_table.php
+++ do_create_table.php
@@ -1,18 +1,22 @@
 <?php
-try{
-$pdo = new PDO('mysql:host=localhost;dbname=portfolio_20190325;charset=utf8', 'root', 'pass');
+try {
+    $pdo = new PDO('mysql:host=localhost;dbname=portfolio_20190325;charset=utf8', 'root', 'pass');
 
-$sql = 'create table users (
-id INT(11),
+    $sql = 'create table users (
+id INT(11) AUTO_INCREMENT PRIMARY KEY,
 username VARCHAR(255),
 password VARCHAR (255)
 ) engine=innodb default charset=utf8';
 
-$res = $pdo->query($sql);
+    $res = $pdo->query($sql);
 
-echo 'テーブルが作成されました';
+    echo '<p>テーブルが作成されました<p>
+        <button onclick="location.href=\'login.php\'">ログイン</button>
 
-} catch(PDOException $e) {
+';
+
+
+} catch (PDOException $e) {
 
     echo $e->getMessage();
     die();
