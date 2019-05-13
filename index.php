f27c016685558fd3bbcd9243add50f2b664d6b94
yasuaki640 <yasuaki640@gmail.com>
2019-05-13 15:32:34 +0900

    ログインチェック機能の実装

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php require_once(dirname(__FILE__) . '/layouts/header.php') ?>
 
+<?php
+//ログインチェック機能
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+}
+
+?>
+
+
 <div class="container-fluid mx-auto my-4" style="max-width: 800px">
     <img src="layouts/images/tsubasa.png">
 </div>
