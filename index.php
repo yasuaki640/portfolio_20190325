7f79fbdcc8519eb9dc0050c23f3f35e68beda646
yasuaki640 <yasuaki640@gmail.com>
2019-05-16 18:37:34 +0900

    グリッドシステムの導入
    bootstrap 4 のグリッドシステムの導入

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php require_once(dirname(__FILE__) . '/layouts/header.php') ?>
 
+<?php require_once(dirname(__FILE__) . '/layouts/header.php') ?>
+
+
 <?php
 //ログインチェック機能
 session_start();
@@ -8,10 +11,24 @@ if (!isset($_SESSION['user'])) {
 }
 ?>
 
+<div class="container-fluid">
 
-<div class="container-fluid mx-auto my-4" style="max-width: 800px">
-    <img src="layouts/images/tsubasa.png">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="container-fluid mx-auto my-4" style="max-width: 800px">
+                    <img src="storage/public/Product/139808064.jpg">
+                </div>
+            </div>
+            <div class="col-md-6">
+
+            </div>
+        </div>
+    </div>
+
+
 </div>
+
 
 <button onclick="location.href='logout.php'">ログアウト</button>
 
