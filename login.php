8ee34bb121f719abc852a47366c3ab33fe8e9401
yasuaki640 <yasuaki640@gmail.com>
2019-05-17 14:59:02 +0900

    レイアウトの微調整

--- login.php
+++ login.php
@@ -1,12 +1,12 @@
 <?php require_once(dirname(__FILE__) . '/layouts/header.php') ?>
 
-    <div class="container-fluid mx-auto my-4" style="max-width: 350px">
-        <form action="do_login.php" method="post">
+    <div class="container-fluid mx-auto my-4 border" style="max-width: 350px">
+        <form action="do_login.php" method="post" class="my-2">
             <p>ユーザーネーム<input type="text" name="username"></p>
             <p>パスワード<input type="password" name="password"></p>
-            <input type="submit" value="ログイン">
+            <button type="submit" class="btn btn-secondary btn-block">ログイン</button>
         </form>
-        <button onclick="location.href='input_user_info.php'">新規登録</button>
+        <button type="button" class="btn btn-primary btn-block mb-2" onclick="location.href='input_user_info.php'">新規登録</button>
     </div>
 
 
