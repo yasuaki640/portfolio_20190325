517b5c5a79f210d6ee08d04de3accf9e06f01f0c
yasuaki640 <yasuaki640@gmail.com>
2019-05-29 19:39:57 +0900

    エラー画面の追加

---
+++ error.php
@@ -0,0 +1,11 @@
+<?php require_once(dirname(__FILE__) . '/layouts/header.php') ?>
+
+<div class="container-fluid" style="max-width: 80%">
+    <p>予期せぬエラーが発生しました。</p>
+</div>
+
+
+<button class="mx-auto d-block" onclick="location.href='login.php'">ログイン</button>
+
+
+<?php require_once(dirname(__FILE__) . '/layouts/footer.php') ?>
--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 //ログインチェック機能
 session_start();
 if (!isset($_SESSION['user'])) {
-    header("Location: login.php");
+    header("Location: error.php");
 }
 ?>
 
